655151e6b48c432901978de85b52a2fe938dbe26
Lokathor <zefria@gmail.com>
2022-08-04 14:15:57 +0000

    Deploying to gh-pages from @ Lokathor/gamecrab@3857c59d29041d3c9b651295234c11678883d44f 🚀

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var sourcesIndex = {};
 sourcesIndex["bitfrob"] = {"name":"","files":["const_region_mask.rs","fns.rs","lib.rs","u8x2_.rs"]};
 sourcesIndex["bytemuck"] = {"name":"","files":["anybitpattern.rs","checked.rs","contiguous.rs","internal.rs","lib.rs","no_uninit.rs","offset_of.rs","pod.rs","pod_in_option.rs","transparent.rs","zeroable.rs","zeroable_in_option.rs"]};
-sourcesIndex["gamecrab"] = {"name":"","files":["cpu.rs","lib.rs"]};
+sourcesIndex["gamecrab"] = {"name":"","files":["cpu.rs","lib.rs","op.rs"]};
 createSourceSidebar();
